f7d6ee10082b69ebcb1642d58d1c8e01866ea6fa
Mark Garnett <thecommunistduck@hotmail.co.uk>
2017-02-10 22:09:26 +0000

    Added driver

---
+++ Assets/Driver.cs
@@ -0,0 +1,65 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class GameEvent
+{
+}
+
+public class Planner
+{
+	public void AddEvent()
+	{
+		Debug.Log ("Added event");
+	}
+	public List<GameEvent> GetEvents()
+	{
+		return new List<GameEvent>();
+	}
+}
+public class Driver : MonoBehaviour {
+	
+	public enum Stage
+	{
+		Planning,
+		DayRunning,
+		Summary
+	}
+
+	public Stage stage;
+	public World world;
+	public Planner planner;
+
+	// Use this for initialization
+	void Start () {
+		stage = Stage.Planning;
+		world = new World ();
+		planner = new Planner ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		switch (stage) {
+		case Stage.Planning:
+			if (!Input.GetKeyDown (KeyCode.A))
+			{
+				return;
+			}
+			planner.AddEvent();
+			Debug.Log("starting day");
+			break;
+		case Stage.DayRunning:
+			SimulateDay();
+			break;
+		}
+	}
+
+	void SimulateDay()
+	{
+		//for each event:
+		foreach(GameEvent g in planner.GetEvents())
+		{
+			world.DoEvent(g);
+		}
+	}
+}
---
+++ Assets/World.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public struct Faction {
+	public string Name;
+	public int Size;
+}
+
+public class Region {
+	public int ID;
+	public string Name;
+	public List<Faction> factions;
+
+	public Region(int id, string name)
+	{
+		this.ID = id;
+		this.Name = name;
+		this.factions = new List<Faction>();
+	}
+
+	public void AddFaction(Faction f)
+	{
+		factions.Add (f);
+	}
+}
+
+public class World {
+	public static int SOMEWHERE_ID = 0;
+
+	public List<Region> regions;
+	public World()
+	{
+		this.regions = new List<Region> ();
+		this.regions.Add(new Region (SOMEWHERE_ID, "Somewhere"));
+		this.regions [SOMEWHERE_ID].AddFaction (new Faction(){Name="Nobles", Size = 100});
+	}
+
+	public void DoEvent(GameEvent g)
+	{
+	}
+}
