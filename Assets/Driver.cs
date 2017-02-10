67c37d22de48b9c1db45d0027c42e9ff02bfb693
Mark Garnett <thecommunistduck@hotmail.co.uk>
2017-02-10 23:06:58 +0000

    Train now explodes

---
+++ Assets/Driver.cs
@@ -0,0 +1,114 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+
+public abstract class GameEvent
+{
+	public static int BLOW_UP_TRAIN = 0;
+	public string Name {get; protected set;}
+
+	public abstract void Do (World w, Region r);
+}
+
+public class BombTrainEvent : GameEvent
+{
+	public BombTrainEvent()
+	{
+		this.Name = "Bomb Train";
+	}
+
+	public override void Do(World w, Region r)
+	{
+		Debug.Log ("Bombed a train in " + r.Name);
+	}
+}
+
+public struct ConcreteEvent
+{
+	public GameEvent Event;
+	public int RegionID;
+
+	public string Name { get { return Event.Name; } }
+
+	public ConcreteEvent(GameEvent g, int r)
+	{
+		this.Event = g;
+		this.RegionID = r;
+	}
+}
+
+public class Planner
+{
+	private List<KeyValuePair<int, int>> eventsToHappen;
+	public Planner()
+	{
+		eventsToHappen = new List<KeyValuePair<int, int>> ();
+	}
+
+	public void AddEvent(int eventid, int regionid)
+	{
+		eventsToHappen.Add (new KeyValuePair<int, int>(eventid, regionid));
+		Debug.Log ("Added event " + Driver.Events[eventid].Name);
+	}
+	public IEnumerable<ConcreteEvent> GetEvents()
+	{
+		return eventsToHappen.Select (t => new ConcreteEvent(Driver.Events [t.Key], t.Value));
+	}
+}
+public class Driver : MonoBehaviour {
+	
+	public enum Stage
+	{
+		Planning,
+		DayRunning,
+		Summary
+	}
+
+	public static Dictionary<int, GameEvent> Events;
+	private Stage stage;
+	public int Day = 1;
+	public World world;
+	public Planner planner;
+
+	// Use this for initialization
+	void Start () {
+		stage = Stage.Planning;
+		world = new World ();
+		planner = new Planner ();
+		Events = new Dictionary<int, GameEvent> ();
+		Events.Add (GameEvent.BLOW_UP_TRAIN, new BombTrainEvent ());
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		switch (stage) {
+		case Stage.Planning:
+			if (!Input.GetKeyDown (KeyCode.A))
+			{
+				return;
+			}
+			planner.AddEvent(GameEvent.BLOW_UP_TRAIN, World.SOTON_ID);
+			stage = Stage.DayRunning;
+			break;
+		case Stage.DayRunning:
+			SimulateDay();
+			stage = Stage.Planning;
+			break;
+		}
+	}
+
+	void SimulateDay()
+	{
+		Debug.Log ("starting day" + Day);
+		//for each event:
+		foreach(ConcreteEvent g in planner.GetEvents())
+		{
+			world.DoEvent(g);
+			planner.ClearEvent();
+		}
+		Debug.Log ("ending day" + Day);
+		Day++;
+	}
+}
---
+++ Assets/World.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public struct Faction {
+	public string Name;
+	public int Size;
+}
+
+public class Region {
+	public int ID;
+	public string Name { get; private set; }
+	public List<Faction> factions;
+
+	public Region(int id, string name)
+	{
+		this.ID = id;
+		this.Name = name;
+		this.factions = new List<Faction>();
+	}
+
+	public void AddFaction(Faction f)
+	{
+		factions.Add (f);
+	}
+}
+
+public class World {
+	public static int SOTON_ID = 0;
+
+	public List<Region> regions;
+	public World()
+	{
+		this.regions = new List<Region> ();
+		this.regions.Add(new Region (SOTON_ID, "Southampton"));
+		this.regions [SOTON_ID].AddFaction (new Faction(){Name="Nobles", Size = 100});
+	}
+
+	public void DoEvent(ConcreteEvent g)
+	{
+		//Debug.Log("Currently executing event " + g.Name);
+		g.Event.Do (this, this.regions [g.RegionID]);
+	}
+}
