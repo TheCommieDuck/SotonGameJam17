242c7d66f17d9e4a6ecbfebb795ba9b3db4aa9bf
tconstantin <ct2u16@soton.ac.uk>
2017-02-10 22:04:59 +0000

    white map

---
+++ Assets/changecolor.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using static System.Net.Mime.MediaTypeNames;
+
+public class changecolor : MonoBehaviour {
+
+    public Image image;
+	// Use this for initialization
+	void Start () {
+		ddd.
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
